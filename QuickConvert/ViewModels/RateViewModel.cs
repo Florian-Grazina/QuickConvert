9c908fdbe0f762f907a99aa28c7e74c807602fa7
Florian Grazina <114757171+Florian-Grazina@users.noreply.github.com>
2025-01-24 22:16:12 +0100

    implements refresh on expiration date

--- QuickConvert/ViewModels/RateViewModel.cs
+++ QuickConvert/ViewModels/RateViewModel.cs
@@ -1,28 +1,57 @@
-﻿using QuickConvert.API.Enums;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using QuickConvert.API.Enums;
 using QuickConvert.Managers;
 using QuickConvert.Models;
+using System.Globalization;
 
 namespace QuickConvert.ViewModels
 {
-    public class RateViewModel(Rate rate)
+    public partial class RateViewModel(Rate rate) : ObservableObject
     {
         #region data members
         private readonly Rate _rate = rate;
         #endregion
 
+        #region observable properties
+        public DateTime Date
+        {
+            get => _rate.LastUpdateTime;
+            set => SetProperty(Date, value, _rate, (r, d) => r.LastUpdateTime = value);
+        }
+        public double RateAmount
+        {
+            get => _rate.LastRateAmount;
+            set => SetProperty(RateAmount, value, _rate, (r, d) => r.LastRateAmount = value);
+        }
+        #endregion
+
         #region properties
-        public DateTime Date => _rate.LastUpdateTime;
         public DateTime ExpirationDate => _rate.LastUpdateTime.AddHours(AppSettingsManager.Instance.NumberOfHoursBeforeRefresh);
 
         public BaseCurrencyCode BaseCurrencyCode => _rate.BaseCurrencyCode;
         public TargetCurrencyCode TargetCurrencyCode => _rate.TargetCurrencyCode;
-
-        public double RateAmount => _rate.LastRateAmount;
         #endregion
 
         public void RefreshRate()
         {
-            RateManager.Instance.RefreshRate(_rate);
+            double newRate = RateManager.Instance.RefreshRate(_rate);
+            RateAmount = newRate;
+            Date = DateTime.Now;
+        }
+
+        public string CalculateRate(CultureInfo cultureInfo, string input, bool isReversed)
+        {
+            if (DateTime.Now > ExpirationDate)
+                RefreshRate();
+
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            _ = !double.TryParse(input, out double inputAmount);
+            double outputAmount = isReversed ? inputAmount / RateAmount : inputAmount * RateAmount;
+
+            string result = outputAmount.ToString("#,#.##", cultureInfo);
+            return string.IsNullOrEmpty(result) ? "0" : result;
         }
     }
 }
